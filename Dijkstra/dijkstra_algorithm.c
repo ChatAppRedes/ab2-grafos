e9bee1d17a71f9847263db21ec8b376698eef81e
ruifernandees <dev.ruifernandes@gmail.com>
2022-02-25 18:20:01 -0300

    feat: Dijkstra
    Co-authored-by: Pedro Augusto <pedro.augusto.dr11@gmail.com>

--- Dijkstra/dijkstra_algorithm.c
+++ Dijkstra/dijkstra_algorithm.c
@@ -1,27 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+#include "../data-structures/Graph/graph.c"
 
-void initializeD(GRAFO *g, int *d, int *p, int*s)
+void initializeDijkstra(Graph *graph, int *distances, int *predecessor, int s)
 {
-    int v;
+    int vertex;
 
-    for (v=0;<g->vertices;v++)
+    for (vertex = 0; vertex < graph->vertices ; vertex++)
     {
-        d[v] = INT_MAX/2;
-        p[v] = -1;
+        distances[vertex] = INT_MAX/2;
+        predecessor[vertex] = -1;
     }
-    d[s] = 0;
+    distances[s] = 0;
 }
 
-void relaxa(GRAFO *g, int *d, int *p, int u, int v)
-{
-    ADJACENCIA *ad = g->adj[u].cab;
-    while(ad && ad->vertice != v)
-        ad = ad->prox;
-    if(d[v] > d[u] + ad->peso)
+int lessDistance(Graph *graph, bool *opened, int *distances) {
+    int i;
+    for(i=0; i <graph->vertices; i++)
     {
-        d[v] = d[u] + ad->peso;
-        p[v] = u;
-    }    
+        if(opened[i]){
+            break;
+        }
+    }
+    if(i == graph->vertices)
+    {
+        return (-1);
+    }
+    int less = i;
+
+    for(i = less + 1; i<graph->vertices; i++)
+    {
+        if(opened[i] && (distances[less]>distances[i]))
+        {
+            less = i;
+        }
+    }
+    return(less);
+}
+
+
+bool existOpen(Graph *graph, int *open)
+{
+    int i;
+
+    for(int i = 0; i<graph->vertices; i++)
+    {
+        if (open[i])
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+void relax(Graph *graph, int *distances, int *predecessor, int u, int v)
+{
+    Adjacency *adjacency = graph->adjacency[u].head;
+    while(adjacency && adjacency->vertex != v)
+        adjacency = adjacency->next;
+    if (adjacency) 
+    {
+        if(distances[v] > distances[u] + adjacency->weight)
+        {
+            distances[v] = distances[u] + adjacency->weight;
+            predecessor[v] = u;
+        }    
+    }
+}
+
+int *dijkstra(Graph *graph, int startingVertex)
+{
+    int *distances = (int *) malloc(graph->vertices * sizeof(int));
+    int predecessors[graph->vertices];
+    bool opened[graph->vertices];
+    initializeDijkstra(graph, distances, predecessors, startingVertex);
+    
+    for (int i = 0; i < graph->vertices; i++) {
+        opened[i] = true;
+    }
+
+    while (existOpen(graph, opened)) {
+        int u = lessDistance(graph, opened, distances);
+        opened[u] = false;
+        Adjacency *adjacency = graph->adjacency[u].head;
+        while (adjacency) {
+            relax(graph, distances, predecessors, u, adjacency->vertex);
+            adjacency = adjacency->next;
+        }
+    }
+    return distances;
 }
---
+++ Dijkstra/example.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../data-structures/Graph/readGraph.c"
+
+int main()
+{
+    readGraph();
+    return 0;
+}
