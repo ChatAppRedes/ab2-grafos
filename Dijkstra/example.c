5657f8c3310161bbf62fecd00a511c20f244d270
ruifernandees <dev.ruifernandes@gmail.com>
2022-02-25 23:14:05 -0300

    feat(example.c): print Dijkstra

--- Dijkstra/example.c
+++ Dijkstra/example.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "../data-structures/Graph/graph.c"
+#include "dijkstra.c"
 
 int main()
 {
     Graph *graph = readGraph();
     printGraph(graph);
+    printf("\n\n");
+    int selectedVertex = 1;
+    int *distances = dijkstra(graph, selectedVertex);
+    printf("DIJKSTRA: ");
+    for (int i = 0; i < 5; i++) {
+        printf("v%d -> v%d: %d\n", selectedVertex, i+1, distances[i]);
+    }
+    printf("\n");
     return 0;
 }
