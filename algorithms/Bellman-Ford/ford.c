2bfa248815b9f79217fd0ba36b7924a64cefefa5
ruifernandees <dev.ruifernandes@gmail.com>
2022-02-27 20:55:12 -0300

    Ford output

--- algorithms/Bellman-Ford/ford.c
+++ algorithms/Bellman-Ford/ford.c
@@ -38,32 +38,32 @@ void relax(Graph *graph, int *distances, int *predecessor, int u, int v)
 int *bellmanFord(Graph *graph, int initialVertex)
 {
     initialVertex--;
-    printf("SETUP DISTANCES\n");
+    // printf("SETUP DISTANCES\n");
     int *distances = (int *) malloc(graph->vertices * sizeof(int));
-    printf("SETUP PREDECESSORS\n");
+    // printf("SETUP PREDECESSORS\n");
     int predecessors[graph->vertices];
-    printf("SETUP\n");
+    // printf("SETUP\n");
     for (int i = 0; i < graph->vertices; i++) {
         distances[i] = INFINITY;
         predecessors[i] = 0;
     }
-    printf("END SETUP\n");
+    // printf("END SETUP\n");
     distances[initialVertex] = 0;
-    printf("FOR I\n");
+    // printf("FOR I\n");
     for (int i = 0; i <= graph->vertices - 1; i++) {
-        printf("FOR I %d times\n", i);
+        // printf("FOR I %d times\n", i);
         Adjacency *adjacency = graph->adjacency[i].head;
         while(adjacency) {
             relax(graph, distances, predecessors, i, adjacency->vertex);
             adjacency = adjacency->next;
         }
     }
-    printf("END FOR I\n");
-    printf("HAS - CYCLE\n");
-    for (int i = 0; i < graph->vertices; i++) {
-        printf("DISTANCES: \n");
-        printf("%d: %d\n", i+1, distances[i]);
-        printf("END DISTANCES: \n");
-    }
+    // printf("END FOR I\n");
+    // printf("HAS - CYCLE\n");
+    // for (int i = 0; i < graph->vertices; i++) {
+    //     printf("DISTANCES: \n");
+    //     printf("%d: %d\n", i+1, distances[i]);
+    //     printf("END DISTANCES: \n");
+    // }
     return distances;
 }
--- algorithms/Bellman-Ford/main.c
+++ algorithms/Bellman-Ford/main.c
@@ -9,9 +9,19 @@ int main(int argc, char *argv[])
     handleArgs(argv);
     FILE *inputFile = fopen(inputFileName, "r");
     Graph *graph = readGraph(inputFile);
-    printGraph(graph);
-    printf("FORD: \n");
-    bellmanFord(graph, initialVertex);
+    int *distances = bellmanFord(graph, initialVertex);
+    FILE *outFile = fopen(outputFileName, "w");
+    if (finalVertex == 0 || showCompleteSolution) {
+        for (int i = 0; i < graph->vertices; i++) {
+            printf("%d:%d ", i+1, distances[i]);
+            fprintf(outFile, "%d:%d ", i+1, distances[i]);
+        }
+        printf("\n");
+    } else {
+        printf("%d\n", distances[finalVertex - 1]);
+        printf("\n");
+        fprintf(outFile, "%d\n", distances[finalVertex - 1]);
+    }
     clean(graph);
     return 0;
 }
